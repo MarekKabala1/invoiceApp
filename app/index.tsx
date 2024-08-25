654465449431e25c57ed887dd242257c78cb74ef
MarekKabala1 <marekkabala1@gmail.com>
2024-08-25 15:55:22 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+	return (
+		<View className='flex-1 items-center justify-center bg-slate-500'>
+			<Text>Hello in the Invoice App</Text>
+		</View>
+	);
+}
