782b7461645b3235a116147be516770e9a87fb0a
MarekKabala1 <marekkabala1@gmail.com>
2024-12-17 06:04:34 +0000

    add calculateInvoiceTotal to file

--- utils/invoiceCalculations.ts
+++ utils/invoiceCalculations.ts
@@ -1,6 +1,7 @@
-import { WorkInformationType } from '@/db/zodSchema';
+import { InvoiceType, WorkInformationType } from '@/db/zodSchema';
 
-export const calculateInvoiceTotals = (workItems: WorkInformationType[], taxRate: number) => {
+//toDo: refactor that
+export const calculateInvoiceWorkItemTotals = (workItems: WorkInformationType[], taxRate: number) => {
   const subtotal = workItems.reduce((sum, item) => {
     const price = parseFloat(item.unitPrice.toString()) || 0;
     return sum + price;
@@ -10,4 +11,17 @@ export const calculateInvoiceTotals = (workItems: WorkInformationType[], taxRate
   const total = subtotal - tax;
 
   return { subtotal, tax, total };
+};
+
+export const calculateInvoiceTotal = (invoices: InvoiceType[]) => {
+  const totalBeforeTax = invoices.reduce((sum, invoice) => sum + invoice.amountBeforeTax, 0);
+  const totalAfterTax = invoices.reduce((sum, invoice) => sum + invoice.amountAfterTax, 0);
+  const taxToPay = totalBeforeTax - totalAfterTax;
+
+
+  return {
+    totalBeforeTax,
+    totalAfterTax,
+    taxToPay,
+  };
 };
