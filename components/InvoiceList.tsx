b6fc2efbbce8db541ec4f1d4f92b6fa988b60981
MarekKabala1 <marekkabala1@gmail.com>
2025-01-29 06:33:35 +0000

    Perf:rewrite co ponent for better performance

--- components/InvoiceList.tsx
+++ components/InvoiceList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity, Alert } from 'react-native';
 import { useFocusEffect, useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
@@ -18,202 +18,201 @@ type NoteType = z.infer<typeof noteSchema>;
 type CustomerSchema = z.infer<typeof customerSchema>;
 
 export default function InvoiceList() {
-	const [invoices, setInvoices] = useState<InvoiceType[]>([]);
-	const [payments, setPayments] = useState<PaymentType[]>([]);
-	const [notes, setNotes] = useState<NoteType[]>([]);
-	const [workItems, setWorkItems] = useState<WorkInformationType[]>([]);
-	const [customer, setCustomer] = useState<CustomerSchema[]>([]);
+	const [data, setData] = useState<{
+		invoices: InvoiceType[];
+		payments: PaymentType[];
+		notes: NoteType[];
+		workItems: WorkInformationType[];
+		customers: CustomerSchema[];
+	}>({
+		invoices: [],
+		payments: [],
+		notes: [],
+		workItems: [],
+		customers: [],
+	});
 	const [error, setError] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
 	const router = useRouter();
 	const { colors } = useTheme();
 
-	const getInvoices = async () => {
-		const getedInvoices = await db.select().from(Invoice);
-
-		const formattedInvoices = getedInvoices.map((invoice) => ({
-			...invoice,
-			userId: invoice.userId!,
-			customerId: invoice.customerId!,
-			invoiceDate: invoice.invoiceDate!,
-			dueDate: invoice.dueDate!,
-			amountAfterTax: invoice.amountAfterTax!,
-			amountBeforeTax: invoice.amountBeforeTax!,
-			taxRate: invoice.taxRate!,
-			pdfPath: invoice.pdfPath!,
-			createdAt: invoice.createdAt!,
-			currency: 'GBP',
-		}));
-
-		setInvoices(formattedInvoices);
-	};
-
-	const getPayments = async () => {
+	const loadData = useCallback(async () => {
+		setIsLoading(true);
 		try {
-			const getedPayments = await db.select().from(Payment);
+			const [invoicesData, paymentsData, notesData, workItemsData, customersData] = await Promise.all([
+				db.select().from(Invoice),
+				db.select().from(Payment),
+				db.select().from(Note),
+				db.select().from(WorkInformation),
+				db.select().from(Customer),
+			]);
 
-			const formattedPayments = getedPayments.map((payment) => ({
-				...payment,
-				invoiceId: payment.invoiceId ?? '',
-				paymentDate: payment.paymentDate ?? '',
-				amountPaid: payment.amountPaid ?? 0,
-				createdAt: payment.createdAt ?? '',
-			}));
-			setPayments(formattedPayments);
+			setData({
+				invoices: invoicesData.map((invoice) => ({
+					...invoice,
+					userId: invoice.userId!,
+					customerId: invoice.customerId!,
+					invoiceDate: invoice.invoiceDate!,
+					dueDate: invoice.dueDate!,
+					amountAfterTax: invoice.amountAfterTax!,
+					amountBeforeTax: invoice.amountBeforeTax!,
+					taxRate: invoice.taxRate!,
+					pdfPath: invoice.pdfPath!,
+					createdAt: invoice.createdAt!,
+					currency: 'GBP',
+				})),
+				payments: paymentsData.map((payment) => ({
+					...payment,
+					invoiceId: payment.invoiceId ?? '',
+					paymentDate: payment.paymentDate ?? '',
+					amountPaid: payment.amountPaid ?? 0,
+					createdAt: payment.createdAt ?? '',
+				})),
+				notes: notesData.map((note) => ({
+					...note,
+					invoiceId: note.invoiceId!,
+					noteDate: note.noteDate!,
+					noteText: note.noteText ?? 'No text',
+					createdAt: note.createdAt!,
+				})),
+				workItems: workItemsData.map((workItem) => ({
+					...workItem,
+					invoiceId: workItem.invoiceId ?? '',
+					descriptionOfWork: workItem.descriptionOfWork ?? 'No description',
+					unitPrice: workItem.unitPrice ?? 0,
+					date: workItem.date ?? '',
+					totalToPayMinusTax: workItem.totalToPayMinusTax ?? 0,
+					createdAt: workItem.createdAt ?? '',
+				})),
+				customers: customersData.map((customer) => ({
+					...customer,
+					emailAddress: customer.emailAddress ?? '', // Ensure non-null string
+					name: customer.name ?? '', // Ensure non-null string
+					id: customer.id,
+					address: customer.address ?? undefined, // Make optional
+					phoneNumber: customer.phoneNumber ?? undefined, // Make optional
+					createdAt: customer.createdAt ?? undefined, // Make optional
+				})),
+			});
 		} catch (error) {
-			console.error('Error geting payments:', error);
-			setError('no payments found');
+			console.error('Error loading data:', error);
+			setError('Failed to load data');
+		} finally {
+			setIsLoading(false);
 		}
-	};
-
-	const getNotes = async () => {
-		try {
-			const getedNotes = await db.select().from(Note);
-			const formattedNotes = getedNotes.map((note) => ({
-				...note,
-				invoiceId: note.invoiceId!,
-				noteDate: note.noteDate!,
-				noteText: note.noteText ?? 'No text',
-				createdAt: note.createdAt!,
-			}));
-			setNotes(formattedNotes);
-		} catch (error) {
-			console.error('Error geting notes:', error);
-			setError('No notes found');
-		}
-	};
-
-	const getWorkInformation = async () => {
-		try {
-			const getedWorkItems = await db.select().from(WorkInformation);
-
-			const formattedWorkItems = getedWorkItems.map((workItem) => ({
-				...workItem,
-				invoiceId: workItem.invoiceId ?? '',
-				descriptionOfWork: workItem.descriptionOfWork ?? 'No description',
-				unitPrice: workItem.unitPrice ?? 0,
-				date: workItem.date ?? '',
-				totalToPayMinusTax: workItem.totalToPayMinusTax ?? 0,
-				createdAt: workItem.createdAt ?? '',
-			}));
-			setWorkItems(formattedWorkItems);
-		} catch (error) {
-			console.error('Error geting work items:', error);
-			setError('No work items found');
-		}
-	};
-
-	const getCustomers = async () => {
-		try {
-			const getedCustomers = await db.select().from(Customer);
-			const formattedCustomers = getedCustomers.map((customer) => ({
-				id: customer.id,
-				name: customer.name ?? '',
-				emailAddress: customer.emailAddress ?? '',
-				phoneNumber: customer.phoneNumber ?? '',
-				address: customer.address ?? '',
-				createdAt: customer.createdAt ?? '',
-			}));
-			setCustomer(formattedCustomers);
-		} catch (error) {
-			console.error('Error geting customers:', error);
-			setError('No customers found');
-		}
-	};
-
-	const loadData = async () => {
-		await getInvoices();
-		await getPayments();
-		await getNotes();
-		await getWorkInformation();
-		await getCustomers();
-	};
-	useEffect(() => {
-		loadData();
 	}, []);
 
 	useFocusEffect(
-		React.useCallback(() => {
+		useCallback(() => {
 			loadData();
-		}, [])
+		}, [loadData])
 	);
 
-	const deleteInvoice = async (invoiceId: string) => {
-		try {
-			await db.transaction(async (tx) => {
-				await tx.delete(WorkInformation).where(eq(WorkInformation.invoiceId, invoiceId));
-				await tx.delete(Payment).where(eq(Payment.invoiceId, invoiceId));
-				await tx.delete(Note).where(eq(Note.invoiceId, invoiceId));
-				await tx.delete(Invoice).where(eq(Invoice.id, invoiceId));
-			});
+	const memoizedInvoices = useMemo(() => {
+		return data.invoices.map((invoice) => {
+			const invoicePayments = data.payments.filter((p) => p.invoiceId === invoice.id);
+			const invoiceNotes = data.notes.filter((n) => n.invoiceId === invoice.id);
+			const invoiceWorkItems = data.workItems.filter((w) => w.invoiceId === invoice.id);
+			const customer = data.customers.find((c) => c.id === invoice.customerId) || {
+				name: 'Unknown',
+				emailAddress: 'unknown@example.com',
+				id: invoice.customerId,
+			};
 
-			await loadData();
-		} catch (error) {
-			console.error('Error deleting invoice:', error);
-			Alert.alert('Error', 'Failed to delete invoice. Please try again.');
-		}
-	};
+			return {
+				...invoice,
+				payments: invoicePayments,
+				notes: invoiceNotes,
+				workItems: invoiceWorkItems,
+				customer,
+			};
+		});
+	}, [data]);
 
-	const handleDeleteInvoice = (invoiceId: string) => {
-		Alert.alert('Delete Invoice', 'Are you sure you want to delete this invoice? This action cannot be undone.', [
-			{ text: 'Cancel', style: 'cancel' },
-			{
-				text: 'Delete',
-				style: 'destructive',
-				onPress: () => deleteInvoice(invoiceId),
-			},
-		]);
-	};
-
-	const handleUpdateInvoice = (invoiceId: string) => {
-		const invoice = invoices.find((inv) => inv.id === invoiceId);
-		const invoiceWorkItems = workItems.filter((item) => item.invoiceId === invoiceId);
-		const invoiceNotes = notes.filter((note) => note.invoiceId === invoiceId);
-		const payment = payments.filter((item) => item.invoiceId === invoiceId);
-
-		if (invoice) {
-			router.push({
-				pathname: '/createInvoice',
-				params: {
-					mode: 'update',
-					invoiceId: invoiceId,
-					invoice: JSON.stringify(invoice),
-					workItems: JSON.stringify(invoiceWorkItems),
-					notes: JSON.stringify(invoiceNotes),
-					payments: JSON.stringify(payment),
-				},
-			});
-		}
-	};
+	if (error) {
+		return (
+			<View>
+				<Text style={{ color: colors.danger }}>{error}</Text>
+			</View>
+		);
+	}
 
 	return (
-		<View className=' flex-1 bg-light-primary dark:bg-dark-primary gap-4 p-4 mb-[68px]'>
-			<View className='w-full justify-between flex-row'>
-				<ThemeToggle size={30} />
-				<TouchableOpacity onPress={() => router.push('/createInvoice')} className='flex-row gap-1 items-center'>
-					<View>
-						<Ionicons name='add-circle-outline' size={28} color={colors.text} />
-					</View>
-					<Text className='text-light-text dark:text-dark-text text-xs font-bold'>Create Invoice</Text>
-				</TouchableOpacity>
+		<View className='flex-1 bg-light-primary dark:bg-dark-primary'>
+			<View className='flex-row justify-between p-4'>
+				<Text className='text-2xl font-bold text-light-text dark:text-dark-text'>Invoices</Text>
+				<ThemeToggle size={24} />
 			</View>
+			{isLoading ? (
+				<View className='flex-1 justify-center items-center'>
+					<Text className='text-light-text dark:text-dark-text'>Loading...</Text>
+				</View>
+			) : (
+				<FlatList
+					data={memoizedInvoices}
+					keyExtractor={(item) => item.id || String(Math.random())}
+					renderItem={({ item }) => (
+						<InvoiceCard
+							invoice={item}
+							workItems={item.workItems}
+							payments={item.payments}
+							notes={item.notes}
+							customer={item.customer}
+							onDelete={() =>
+								Alert.alert('Delete Invoice', 'Are you sure you want to delete this invoice? This action cannot be undone.', [
+									{ text: 'Cancel', style: 'cancel' },
+									{
+										text: 'Delete',
+										style: 'destructive',
+										onPress: async () => {
+											try {
+												await db.transaction(async (tx) => {
+													if (!item.id) {
+														throw new Error('Invoice ID is undefined');
+													}
+													await tx.delete(WorkInformation).where(eq(WorkInformation.invoiceId, item.id as string));
+													await tx.delete(Payment).where(eq(Payment.invoiceId, item.id as string));
+													await tx.delete(Note).where(eq(Note.invoiceId, item.id as string));
+													await tx.delete(Invoice).where(eq(Invoice.id, item.id as string));
+												});
 
-			<FlatList
-				data={invoices}
-				renderItem={({ item }) => (
-					<InvoiceCard
-						invoice={item}
-						workItems={workItems.filter((workItem) => workItem.invoiceId === item.id)}
-						payments={payments.filter((payment) => payment.invoiceId === item.id)}
-						notes={notes.filter((note) => note.invoiceId === item.id)}
-						customer={customer.filter((c) => c.id === item.customerId)}
-						key={item.id}
-						onDelete={handleDeleteInvoice}
-						onUpdate={handleUpdateInvoice}
-					/>
-				)}
-				keyExtractor={(item) => item.id as string}
-				ListEmptyComponent={<Text className='text-center mt-4 text-mutedForeground'>No invoices found</Text>}
-			/>
+												await loadData();
+											} catch (error) {
+												console.error('Error deleting invoice:', error);
+												Alert.alert('Error', 'Failed to delete invoice. Please try again.');
+											}
+										},
+									},
+								])
+							}
+							onUpdate={() => {
+								const invoice = item;
+								const invoiceWorkItems = item.workItems;
+								const invoiceNotes = item.notes;
+								const payment = item.payments;
+
+								router.push({
+									pathname: '/createInvoice',
+									params: {
+										mode: 'update',
+										invoiceId: invoice.id,
+										invoice: JSON.stringify(invoice),
+										workItems: JSON.stringify(invoiceWorkItems),
+										notes: JSON.stringify(invoiceNotes),
+										payments: JSON.stringify(payment),
+									},
+								});
+							}}
+						/>
+					)}
+					contentContainerStyle={{ padding: 16 }}
+				/>
+			)}
+			<TouchableOpacity
+				className='absolute bottom-4 right-4 bg-light-primary dark:bg-dark-primary rounded-full w-[60px] h-[60px] justify-center items-center'
+				onPress={() => router.push('/createInvoice')}>
+				<Ionicons name='add' size={30} color={colors.primary} />
+			</TouchableOpacity>
 		</View>
 	);
 }
