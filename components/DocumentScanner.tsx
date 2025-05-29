36d3be6a3d495ce16c8ed30ffe2cfa620d77d364
MarekKabala1 <marekkabala1@gmail.com>
2025-05-29 06:31:48 +0100

    feat(DocumentScanner): integrate budget confirmation modal and update scan completion handling

--- components/DocumentScanner.tsx
+++ components/DocumentScanner.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, Alert, Modal, TextInput } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useCameraScanner } from '@/hooks/useCameraScanner';
 import { useTheme } from '@/context/ThemeContext';
 import BaseCard from './BaseCard';
+import AddTransactionAfterScan from './AddTransactionAfterScann';
 
 interface DocumentScannerProps {
 	onScanComplete?: (data: { pages: string[]; pdf?: string | null }) => void;
@@ -12,7 +13,9 @@ interface DocumentScannerProps {
 const DocumentScanner: React.FC<DocumentScannerProps> = ({ onScanComplete }) => {
 	const { colors, isDark } = useTheme();
 	const { scannedData, handleScan, isLoading, resetScanDirectory } = useCameraScanner();
-	const [scannedDataVisibleInUi, setScannedDataVisibleInUi] = useState(false);
+	const [scannedDataConfirmationVisibleInUi, setScannedDataConfirmationVisibleInUi] = useState(false);
+	const [isAddToBudgetModalVisible, setIsAddToBudgetModalVisible] = useState(false);
+
 	const handleScanPress = async () => {
 		await handleScan();
 		if (scannedData && onScanComplete) {
@@ -22,12 +25,20 @@ const DocumentScanner: React.FC<DocumentScannerProps> = ({ onScanComplete }) => 
 
 	useEffect(() => {
 		if (scannedData) {
-			setScannedDataVisibleInUi(true);
+			setScannedDataConfirmationVisibleInUi(true);
+			handleAddToBudget();
 			setTimeout(() => {
-				setScannedDataVisibleInUi(false);
-			}, 2000);
+				setScannedDataConfirmationVisibleInUi(false);
+			}, 1000);
 		}
 	}, [scannedData]);
+
+	const handleAddToBudget = () => {
+		Alert.alert('Add to Budget', 'Add the scanned data to the budget?', [
+			{ text: 'Cancel', style: 'cancel' },
+			{ text: 'Add', style: 'destructive', onPress: () => setIsAddToBudgetModalVisible(true) },
+		]);
+	};
 
 	const handleReset = () => {
 		Alert.alert('Reset Directory', 'Reset scan directory location?', [
@@ -37,30 +48,35 @@ const DocumentScanner: React.FC<DocumentScannerProps> = ({ onScanComplete }) => 
 	};
 
 	return (
-		<View className='flex gap-3'>
-			<BaseCard className='p-4'>
-				{scannedDataVisibleInUi ? (
-					<Text className='text-success text-center text-lg font-bold'>✓ Scan Completed</Text>
-				) : (
-					<TouchableOpacity onPress={handleScanPress} disabled={isLoading}>
-						<View className='flex-row items-center justify-center'>
-							<MaterialCommunityIcons name={isLoading ? 'loading' : 'camera-outline'} size={24} color='white' className='mr-2' />
-							<Text className='text-white text-lg font-semibold'>{isLoading ? 'Scanning...' : 'Scan Document'}</Text>
-						</View>
-					</TouchableOpacity>
-				)}
-			</BaseCard>
+		<>
+			<View className='flex gap-3'>
+				<BaseCard className='p-4'>
+					{scannedDataConfirmationVisibleInUi ? (
+						<Text className='text-success text-center text-lg font-bold'>✓ Scan Completed</Text>
+					) : (
+						<TouchableOpacity onPress={handleScanPress} disabled={isLoading}>
+							<View className='flex-row items-center justify-center'>
+								<MaterialCommunityIcons name={isLoading ? 'loading' : 'camera-outline'} size={24} color='white' className='mr-2' />
+								<Text className='text-white text-lg font-semibold'>{isLoading ? 'Scanning...' : 'Scan Document'}</Text>
+							</View>
+						</TouchableOpacity>
+					)}
+				</BaseCard>
 
-			<BaseCard className='p-4'>
-				<TouchableOpacity onPress={handleReset} className='flex-row items-center justify-center'>
-					<MaterialCommunityIcons name='folder-cog-outline' size={20} color={colors.text} className='mr-2' />
-					<Text className='text-light-text dark:text-dark-text'>Reset Scan Directory</Text>
-				</TouchableOpacity>
-			</BaseCard>
-			<Text className='text-xs text-center text-light-text dark:text-dark-text opacity-50'>
-				*To setup new folder for documents, please reset the directory.
-			</Text>
-		</View>
+				<BaseCard className='p-4'>
+					<TouchableOpacity onPress={handleReset} className='flex-row items-center justify-center'>
+						<MaterialCommunityIcons name='folder-cog-outline' size={20} color={colors.text} className='mr-2' />
+						<Text className='text-light-text dark:text-dark-text'>Reset Scan Directory</Text>
+					</TouchableOpacity>
+				</BaseCard>
+				<Text className='text-xs text-center text-light-text dark:text-dark-text opacity-50'>
+					*To setup new folder for documents, please reset the directory.
+				</Text>
+			</View>
+			<Modal visible={isAddToBudgetModalVisible} transparent={true} animationType='slide'>
+				<AddTransactionAfterScan isAddToBudgetModalVisible={isAddToBudgetModalVisible} closeModal={() => setIsAddToBudgetModalVisible(false)} />
+			</Modal>
+		</>
 	);
 };
 
