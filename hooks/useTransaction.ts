9915535ae7600a645f8ac7b1b1805cc9303b8f16
MarekKabala1 <marekkabala1@gmail.com>
2025-05-29 06:27:36 +0100

    feat(currency):add currency to the custom hook

--- hooks/useTransaction.ts
+++ hooks/useTransaction.ts
@@ -3,9 +3,19 @@ import { Alert } from 'react-native';
 import { db } from '@/db/config';
 import { User } from '@/db/schema';
 import { TransactionType } from '@/db/zodSchema';
+import currencyData from '@/assets/currency.json';
 
 export const useTransaction = () => {
   const [users, setUsers] = useState<{ label: string; value: string }[]>([]);
+  const [currencies, setCurrencies] = useState<{ label: string; value: string }[]>([]);
+
+  const getCurrencies = async () => {
+    const currencies = currencyData.map((currency) => ({
+      label: currency.currencyName,
+      value: currency.currencyCode,
+    }));
+    setCurrencies(currencies);
+  };
 
   const getUsers = async () => {
     try {
@@ -22,10 +32,15 @@ export const useTransaction = () => {
   };
 
   useEffect(() => {
-    getUsers();
+    const initializeData = async () => {
+      await Promise.all([getUsers(), getCurrencies()]);
+    };
+
+    initializeData();
   }, []);
 
   return {
     users,
+    currencies,
   };
 };
