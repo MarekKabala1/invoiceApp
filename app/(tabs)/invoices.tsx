0ce66dd4a87be6fe267f48bc84785502f4c2458a
MarekKabala1 <marekkabala1@gmail.com>
2025-06-07 15:46:04 +0100

    refactor(Invoices): replace View with ScrollView for improved layout and scrolling functionality in invoice display

--- app/(tabs)/invoices.tsx
+++ app/(tabs)/invoices.tsx
@@ -1,14 +1,14 @@
 import InvoiceList from '@/components/InvoiceForm/InvoiceList';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Invoices() {
 	const insets = useSafeAreaInsets();
 	return (
 		<SafeAreaView className='flex-1 bg-light-primary dark:bg-dark-primary' style={{ paddingTop: insets.top }}>
-			<View className='flex-1 bg-light-primary dark:bg-dark-primary'>
+			<ScrollView className='flex-1 bg-light-primary dark:bg-dark-primary'>
 				<InvoiceList />
-			</View>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
