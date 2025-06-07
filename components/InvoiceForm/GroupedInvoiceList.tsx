df786a6a3fdbb23d32bbb3ac9a0adafe5a838595
MarekKabala1 <marekkabala1@gmail.com>
2025-06-07 16:17:46 +0100

    refactor(GroupedInvoiceList):fix bug VirtualizedLists should never be nested inside plain ScrollViews

--- components/InvoiceForm/GroupedInvoiceList.tsx
+++ components/InvoiceForm/GroupedInvoiceList.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, Alert, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@/context/ThemeContext';
 import InvoiceCard from './InvoiceCard';
@@ -34,62 +34,65 @@ export default function GroupedInvoiceList({
 		}));
 	};
 
-	console.log(groupedInvoices.length);
+	const renderGroup = ({ item: group }: { item: GroupedInvoice }) => {
+		const groupKey = `${group.year}-${group.month}`;
+		const isExpanded = expandedGroups[groupKey];
+
+		return (
+			<View>
+				<TouchableOpacity
+					onPress={() => toggleGroup(groupKey)}
+					className={`flex-row justify-between items-center p-2 ${isExpanded ? '' : ' border-b border-light-text/20 dark:border-dark-text/20'}`}>
+					<Text className='text-light-text dark:text-dark-text font-bold'>
+						{group.month} {group.year} ({group.invoices.length})
+					</Text>
+					<Ionicons name={isExpanded ? 'chevron-up' : 'chevron-down'} size={24} color={colors.text} />
+				</TouchableOpacity>
+
+				{isExpanded && (
+					<View className={`${isExpanded ? ' border-b border-light-text/20 dark:border-dark-text/20' : ''}`}>
+						{group.invoices.map((invoice) => (
+							<View key={invoice.id} className='flex-row items-center'>
+								<InvoiceCard
+									invoice={invoice}
+									onAdd={addInvoiceToBudget}
+									workItems={invoice.workItems}
+									payments={invoice.payments}
+									notes={invoice.notes}
+									customer={invoice.customer}
+									onDelete={() =>
+										Alert.alert('Delete Invoice', 'Are you sure you want to delete this invoice? This action cannot be undone.', [
+											{ text: 'Cancel', style: 'cancel' },
+											{
+												text: 'Delete',
+												style: 'destructive',
+												onPress: () => onDelete(invoice.id),
+											},
+										])
+									}
+									onUpdate={(id, updateData) => onUpdate(invoice, updateData)}
+								/>
+								{addInvoiceToBudget && (
+									<View>
+										<TouchableOpacity onPress={() => onToggleSelection(invoice.id)} className='p-2'>
+											<Ionicons name={selectedInvoices.includes(invoice.id) ? 'checkbox' : 'square-outline'} size={24} color={colors.text} />
+										</TouchableOpacity>
+									</View>
+								)}
+							</View>
+						))}
+					</View>
+				)}
+			</View>
+		);
+	};
 
 	return (
-		<View>
-			{groupedInvoices.map((group) => {
-				const groupKey = `${group.year}-${group.month}`;
-				const isExpanded = expandedGroups[groupKey];
-
-				return (
-					<View key={groupKey} className=''>
-						<TouchableOpacity
-							onPress={() => toggleGroup(groupKey)}
-							className={`flex-row justify-between items-center p-2  ${isExpanded ? '' : ' border-b border-light-text/20 dark:border-dark-text/20'}`}>
-							<Text className='text-light-text dark:text-dark-text font-bold'>
-								{group.month} {group.year} ({group.invoices.length})
-							</Text>
-							<Ionicons name={isExpanded ? 'chevron-up' : 'chevron-down'} size={24} color={colors.text} />
-						</TouchableOpacity>
-
-						{isExpanded && (
-							<View className={`${isExpanded ? ' border-b border-light-text/20 dark:border-dark-text/20' : ''}`}>
-								{group.invoices.map((invoice) => (
-									<View key={invoice.id} className='flex-row items-center'>
-										<InvoiceCard
-											invoice={invoice}
-											onAdd={addInvoiceToBudget}
-											workItems={invoice.workItems}
-											payments={invoice.payments}
-											notes={invoice.notes}
-											customer={invoice.customer}
-											onDelete={() =>
-												Alert.alert('Delete Invoice', 'Are you sure you want to delete this invoice? This action cannot be undone.', [
-													{ text: 'Cancel', style: 'cancel' },
-													{
-														text: 'Delete',
-														style: 'destructive',
-														onPress: () => onDelete(invoice.id),
-													},
-												])
-											}
-											onUpdate={(id, updateData) => onUpdate(invoice, updateData)}
-										/>
-										{addInvoiceToBudget && (
-											<View>
-												<TouchableOpacity onPress={() => onToggleSelection(invoice.id)} className='p-2'>
-													<Ionicons name={selectedInvoices.includes(invoice.id) ? 'checkbox' : 'square-outline'} size={24} color={colors.text} />
-												</TouchableOpacity>
-											</View>
-										)}
-									</View>
-								))}
-							</View>
-						)}
-					</View>
-				);
-			})}
-		</View>
+		<FlatList
+			data={groupedInvoices}
+			renderItem={renderGroup}
+			keyExtractor={(item) => `${item.year}-${item.month}`}
+			contentContainerStyle={{ paddingBottom: 16 }}
+		/>
 	);
 }
