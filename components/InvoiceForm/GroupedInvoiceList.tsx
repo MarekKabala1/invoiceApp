f2c4ec0ce2d2fb30c4f2dbe99d23d443c4242228
MarekKabala1 <marekkabala1@gmail.com>
2025-07-02 06:15:36 +0100

    style(css): change color unpaid if > 0 green else red

--- components/InvoiceForm/GroupedInvoiceList.tsx
+++ components/InvoiceForm/GroupedInvoiceList.tsx
@@ -10,7 +10,10 @@ import { InvoiceForUpdate } from '@/types';
 interface GroupedInvoiceListProps {
 	groupedInvoices: GroupedInvoice[];
 	onDelete: (invoiceId: string) => void;
-	onUpdate: (invoice: InvoiceForUpdate, updateData?: Partial<InvoiceType>) => void;
+	onUpdate: (
+		invoice: InvoiceForUpdate,
+		updateData?: Partial<InvoiceType>
+	) => void;
 	onToggleSelection: (invoiceId: string) => void;
 	selectedInvoices: string[];
 	addInvoiceToBudget: boolean;
@@ -24,7 +27,9 @@ export default function GroupedInvoiceList({
 	selectedInvoices,
 	addInvoiceToBudget,
 }: GroupedInvoiceListProps) {
-	const [expandedGroups, setExpandedGroups] = useState<{ [key: string]: boolean }>({});
+	const [expandedGroups, setExpandedGroups] = useState<{
+		[key: string]: boolean;
+	}>({});
 	const { colors } = useTheme();
 
 	const toggleGroup = (key: string) => {
@@ -48,15 +53,26 @@ export default function GroupedInvoiceList({
 							{group.month} {group.year}
 						</Text>
 						<View className={`p-2 ${isExpanded ? 'hidden' : 'flex'}`}>
-							<Text className='text-light-text dark:text-dark-text text-xs'>Total:{group.invoices.length}</Text>
-							<Text className='text-danger text-xs'>Unpaid: {group.invoices.filter((invoice) => !invoice.isPayed).length}</Text>
+							<Text className='text-light-text dark:text-dark-text text-xs'>
+								Total:{group.invoices.length}
+							</Text>
+							<Text
+								className={`${group.invoices.filter((invoice) => !invoice.isPayed).length > 0 ? 'text-danger' : 'text-success'} text-xs`}>
+								Unpaid:{' '}
+								{group.invoices.filter((invoice) => !invoice.isPayed).length}
+							</Text>
 						</View>
 					</View>
-					<Ionicons name={isExpanded ? 'chevron-up' : 'chevron-down'} size={24} color={colors.text} />
+					<Ionicons
+						name={isExpanded ? 'chevron-up' : 'chevron-down'}
+						size={24}
+						color={colors.text}
+					/>
 				</TouchableOpacity>
 
 				{isExpanded && (
-					<View className={`${isExpanded ? ' border-b border-light-text/20 dark:border-dark-text/20' : ''}`}>
+					<View
+						className={`${isExpanded ? ' border-b border-light-text/20 dark:border-dark-text/20' : ''}`}>
 						{group.invoices.map((invoice) => (
 							<View key={invoice.id} className='flex-row items-center'>
 								<InvoiceCard
@@ -67,21 +83,35 @@ export default function GroupedInvoiceList({
 									notes={invoice.notes}
 									customer={invoice.customer}
 									onDelete={() =>
-										Alert.alert('Delete Invoice', 'Are you sure you want to delete this invoice? This action cannot be undone.', [
-											{ text: 'Cancel', style: 'cancel' },
-											{
-												text: 'Delete',
-												style: 'destructive',
-												onPress: () => onDelete(invoice.id),
-											},
-										])
+										Alert.alert(
+											'Delete Invoice',
+											'Are you sure you want to delete this invoice? This action cannot be undone.',
+											[
+												{ text: 'Cancel', style: 'cancel' },
+												{
+													text: 'Delete',
+													style: 'destructive',
+													onPress: () => onDelete(invoice.id),
+												},
+											]
+										)
 									}
 									onUpdate={(id, updateData) => onUpdate(invoice, updateData)}
 								/>
 								{addInvoiceToBudget && (
 									<View>
-										<TouchableOpacity onPress={() => onToggleSelection(invoice.id)} className='p-2'>
-											<Ionicons name={selectedInvoices.includes(invoice.id) ? 'checkbox' : 'square-outline'} size={24} color={colors.text} />
+										<TouchableOpacity
+											onPress={() => onToggleSelection(invoice.id)}
+											className='p-2'>
+											<Ionicons
+												name={
+													selectedInvoices.includes(invoice.id)
+														? 'checkbox'
+														: 'square-outline'
+												}
+												size={24}
+												color={colors.text}
+											/>
 										</TouchableOpacity>
 									</View>
 								)}
